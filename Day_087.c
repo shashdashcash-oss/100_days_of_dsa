c51afd061c9a5bd5953ed03766ee183749a9ccb8
Shashwat Tripathi <shashwat@example.com>
2026-04-24 10:00:00 +0530

    Day 087 - Binary Search Iterative: sort array then search target using low mid high pointers

---
+++ Day_087.c
@@ -0,0 +1,49 @@
+// Day 87 - Question 1: Implement Binary Search Iterative
+// Topic: Binary Search on Answer
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - 1 - i; j++) {
+            if(arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int binarySearch(int arr[], int n, int target) {
+    int low = 0, high = n - 1;
+
+    while(low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if(arr[mid] == target)
+            return mid;
+        else if(arr[mid] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    return -1;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    // Sort first, then output sorted array
+    bubbleSort(arr, n);
+
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
